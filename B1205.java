008e39f84d1873912fca7e134cdc9bad1e2d7963
kwondh <100909553+kwondh5217@users.noreply.github.com>
2023-09-05 15:39:22 +0900

    1205 등수 구하기

--- B1205.java
+++ B1205.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class ㅠ1205 {
+public class B1205 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
